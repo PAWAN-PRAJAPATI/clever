c945af6561eb1721f839b156b1fb7e8712556118
PAWAN-PRAJAPATI <prajapatipawan6464@gmail.com>
2018-05-26 21:13:07 +0530

    Update todoListRoutes.js

--- api/routes/todoListRoutes.js
+++ api/routes/todoListRoutes.js
@@ -2,6 +2,12 @@ module.exports = function(app) {
     var todoList = require('../controllers/todoListController');
   
     // todoList Routes
+    app.all('/tasksget', function(req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "X-Requested-With");
+      next();
+     });
+    
     app.route('/getall')
       .get(todoList.create_a_task);
     
