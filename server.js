8bdf1952a3114f1c6bb57434e6bd73da246e2613
PAWAN-PRAJAPATI <prajapatipawan6464@gmail.com>
2018-05-28 20:34:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ bodyParser = require('body-parser');
 
 
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://u7ehgyjt3zarc3h:lzdlv20kbLLYtJlekg43@bdgt0ets7z2gj71-mongodb.services.clever-cloud.com:27017/bdgt0ets7z2gj71";
+var url =MONGO.url
 
 // mongoose instance connection url connection
 MongoClient.connect(url, function(err, db) {
