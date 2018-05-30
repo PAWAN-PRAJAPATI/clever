a922d0a8698f6e3de8a10e5e8ec06afd88127f6e
pawan <PAWAN-PRAJAPATI>
2018-05-30 22:25:19 +0530

    Auth test

---
+++ keys.js
@@ -0,0 +1,14 @@
+const keys = {
+    MONGO:{
+        url:"mongodb://u7ehgyjt3zarc3h:lzdlv20kbLLYtJlekg43@bdgt0ets7z2gj71-mongodb.services.clever-cloud.com:27017/bdgt0ets7z2gj71"
+    },
+    COOKIE:{
+        key:"Iampawan"
+    },
+    GOOGLE:{
+        clientID:'208477398781-aoee5f5c8ru03jthiile1trjah0egb7e.apps.googleusercontent.com'
+        ,clientscrete:'ufPvTKRsGu3y9A4GlUyLETBf'
+    }
+}
+
+module.exports = keys
